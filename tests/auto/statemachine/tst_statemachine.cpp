84dc0ac0e369f0d0fced6c926eed652d7758a12b
Marc Mutz <marc.mutz@qt.io>
2025-06-03 23:19:27 +0200

    Normalize signal/slot signatures 2025
    This is the result of running util/normalize on the code base. The
    following manual edits were needed:
    
    (none)
    
    Pick-to: 6.8 6.5
    Change-Id: I1faaade7c727ac8fe562621f14c1c105020a3840
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit 84dfac09c606a89e882cdcbe4a4fa79398d55f59)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit f98528a71e98ba50d59b2b560825a083e393845e)

--- tests/auto/statemachine/tst_statemachine.cpp
+++ tests/auto/statemachine/tst_statemachine.cpp
@@ -539,7 +539,7 @@ void tst_StateMachine::bindings()
     // -- QScxmlStateMachine::invokedServices
     // Test executes statemachine and observes as the invoked services change
     TopMachine topSm;
-    QSignalSpy invokedSpy(&topSm, SIGNAL(invokedServicesChanged(const QList<QScxmlInvokableService *>)));
+    QSignalSpy invokedSpy(&topSm, SIGNAL(invokedServicesChanged(QList<QScxmlInvokableService*>)));
     QCOMPARE(topSm.invokedServices().size(), 0);
     // at some point during the topSm execution there are 3 invoked services
     topSm.start();
